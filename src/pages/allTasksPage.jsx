7a8e0d6a1e608a1e90f2a6c15f84cf1ecbe4739d
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-12 22:27:31 +0530

    Resolve scrolling if content overflows

--- src/pages/allTasksPage.jsx
+++ src/pages/allTasksPage.jsx
@@ -6,10 +6,12 @@ function AllTasksPage() {
   return (
     <>
       <Nav />
-      <div className="pt-20 sm:pt-24 flex bg-light-background dark:bg-gray-900 min-h-screen">
+      <div className="pt-20 sm:pt-24 flex bg-light-background dark:bg-gray-900 h-screen overflow-hidden">
         <LeftNav />
-        <div className="flex-1 px-4 sm:px-6 lg:px-8">
-          <TaskList />
+        <div className="flex-1 px-4 sm:px-6 lg:px-8 overflow-y-auto">
+          <div className="h-full">
+            <TaskList />
+          </div>
         </div>
       </div>
     </>
