9ccb542ccd5907404045ac1b19883e0a2af823b4
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-12 21:09:25 +0530

    Add service for task summary

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -6,9 +6,19 @@ export const getNoteSummary = async ({ noteId, title, content }) => {
       title,
       content,
     });
-    return response.data;
+    return response?.data;
   } catch (err) {
     const msg = err.response?.data?.message || "Error fetching note summary";
+    throw new Error(msg);
+  }
+};
+
+export const getTaskSummary = async ({ taskId }) => {
+  try {
+    const response = await axios.get(`/ai/tasks/${taskId}/summarize`);
+    return response.data?.data?.summary;
+  } catch (err) {
+    const msg = err.response?.data?.message || "Error fetching task summary";
     throw new Error(msg);
   }
 };
